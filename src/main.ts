9d95578463018072135a6b8160a8b6f562903da3
Tiago M Palte <tiagompalte@hotmail.com>
2022-01-20 19:04:57 -0300

    chore: initial config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Finance Control API')
+    .setDescription(
+      'Edição #2: Desafio Back-End Crie uma API Rest funcional focada em controle financeiro'
+    )
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
